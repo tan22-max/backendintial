7018f589f3e05c46b7f72bd4acb7793d278bbb78
tan22-max <184149842+tan22-max@users.noreply.github.com>
2026-06-30 16:32:28 +0530

    utilcode discusiion

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("mogo db connectuion falied !!!", err);
+})
 
 
 
